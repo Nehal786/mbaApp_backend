0b5cd6fb8db4e4c14f1c9b3e28d5ba60dc6623ee
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2023-04-08 10:39:18 +0530

    verified theatre

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,15 +1,16 @@
 const { getAllTheatres,getTheatreBasedOnId, createTheatre, updateTheatre, deleteTheatre,updateMoviesInTheatre, checkMovieInTheatre } = require("../controllers/theatre.controller");
 const {validateTheatreReqBody } = require("../middleware/verifyTheatreReqBody");
+const { verifyToken } = require("../middleware");
 
 
 
 
 module.exports = (app) => {
-    app.get("/mba/api/v1/theatres",getAllTheatres)
-     app.get("/mba/api/v1/theatre/:id",getTheatreBasedOnId)
-    app.post("/mba/api/v1/theatre/", validateTheatreReqBody , createTheatre);
-    app.put("/mba/api/v1/theatre/:id",updateTheatre);
-    app.delete("/mba/api/v1/theatre/:id",deleteTheatre);
-    app.put("/mba/api/v1/theatre/:id/movies",updateMoviesInTheatre);
-    app.get('/mba/api/v1/theatres/:theatreId/movies/:movieId', checkMovieInTheatre);
+    app.get("/mba/api/v1/theatres",verifyToken ,getAllTheatres)
+     app.get("/mba/api/v1/theatre/:id",verifyToken ,getTheatreBasedOnId)
+    app.post("/mba/api/v1/theatre/", [verifyToken ,validateTheatreReqBody ], createTheatre);
+    app.put("/mba/api/v1/theatre/:id",[verifyToken ,validateTheatreReqBody],updateTheatre);
+    app.delete("/mba/api/v1/theatre/:id",verifyToken ,deleteTheatre);
+    app.put("/mba/api/v1/theatre/:id/movies",verifyToken ,updateMoviesInTheatre);
+    app.get('/mba/api/v1/theatres/:theatreId/movies/:movieId', verifyToken ,checkMovieInTheatre);
 }
