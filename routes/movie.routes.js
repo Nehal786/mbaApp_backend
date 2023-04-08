352dfc64d313332381cafc37bf34877748556f90
Nehal Ahmed Ansari <nehalahmedansari0101@gmail.com>
2023-04-08 10:53:13 +0530

    authenticated movie routes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,12 +1,12 @@
 const { getAllmovies,getMovieBasedOnId, createMovie, updateMovie, deleteMovie } = require("../controllers/movie.controller");
-const {validateMovieReqBody,verifyToken }= require("../middleware/index");
+const {validateMovieReqBody,verifyToken, isAdmin }= require("../middleware/index");
 
 
 
 module.exports = (app) => {
     app.get("/mba/api/v1/movies",verifyToken ,getAllmovies)
-    app.get("/mba/api/v1/movie/:id",getMovieBasedOnId)
-    app.post("/mba/api/v1/movie/", validateMovieReqBody, createMovie);
-    app.put("/mba/api/v1/movie/:id",validateMovieReqBody,updateMovie);
-    app.delete("/mba/api/v1/movie/:id",deleteMovie);
+    app.get("/mba/api/v1/movie/:id",verifyToken ,getMovieBasedOnId)
+    app.post("/mba/api/v1/movie/",[verifyToken ,isAdmin ,validateMovieReqBody], createMovie);
+    app.put("/mba/api/v1/movie/:id",[verifyToken ,isAdmin ,validateMovieReqBody],updateMovie);
+    app.delete("/mba/api/v1/movie/:id",[verifyToken,isAdmin],deleteMovie);
 }
